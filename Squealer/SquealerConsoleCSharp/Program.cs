a61e4a3f1644c5a0502bd4317306d86062c56e45
Chun Leung <chun.leung@aging.ca.gov>
2024-04-04 08:35:37 -0700

    added new project

---
+++ Squealer/SquealerConsoleCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace SquealerConsoleCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
